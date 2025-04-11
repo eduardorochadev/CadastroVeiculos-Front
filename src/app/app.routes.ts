6dc7f1b8b6c086212674af42539657dbf2a6a57c
eduardorochadev <chicoedumacedo@gmail.com>
2025-04-11 02:09:57 -0300

    correção: identação e responsividade

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 import { VehicleDetailComponent } from './vehicle-detail/vehicle-detail.component';
 
 export const routes: Routes = [
-    { path: '', component: VehicleListComponent }, 
+  { path: '', component: VehicleListComponent }, 
   { path: 'add', component: VehicleFormComponent }, 
   { path: 'edit/:id', component: VehicleFormComponent }, 
   { path: 'detail/:id', component: VehicleDetailComponent }, 
